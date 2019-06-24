b1db3db2ee554a1bdb2f9c565f816902321e10f3
rokib <rokib@emailku.com>
2019-06-24 15:45:35 +0700

    publish file index

--- application/controllers/Spirit8.php
+++ application/controllers/Spirit8.php
@@ -3,8 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Spirit8 extends CI_Controller {
 
-        public function index()
-        {
-                $this->load->view('view_spirit8');
-        }
+    public function __construct(){
+        parent::__construct();
+        $this->load->helper('url');
+    }
+
+
+    public function index(){
+        $this->load->view('view_spirit8');
+    }
 }
