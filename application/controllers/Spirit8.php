212e3a4732c58248f971574e8515ba7865d56f32
rokib <rokib@emailku.com>
2019-06-24 08:58:07 +0700

    publish file index

---
+++ application/controllers/Spirit8.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Spirit8 extends CI_Controller {
+
+        public function index()
+        {
+                $this->load->view('view_spirit8');
+        }
+}
