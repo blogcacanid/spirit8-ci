94870d021a87efb0068782ef6e804f70a1b4460b
rokib <rokib@emailku.com>
2019-06-27 21:53:35 +0700

    publish file index

---
+++ application/libraries/Apps.php
@@ -0,0 +1,28 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Apps {
+    var $title = "Belajar CodeIgniter 3"; // Judul untuk aplikasi
+    var $release = "Version 3.0.1"; // Versi dari aplikasi
+    var $project_code = "BC3"; // untuk  penulisan Title yang lebih pendek
+    var $ver = "v3.0.1"; // untuk penulisan versi yang lebih pendek
+
+    var $copyright = "Copyright &copy;";
+    var $company_name = "PT. CACAN";
+    var $company_prod = "PTCCN";
+    var $company_site = "http://www.cacan.id";
+    var $company_logo = "https://i.imgur.com/oAkbQPS.png";
+    var $company_address = "Jl. Muria Ujung No. 63 <br />
+                            Pasar Manggis, Setiabudi Jakarta Selatan Daerah Khusus Ibukota <br />
+                            Jakarta 12970, Indonesia <br />
+                            Support : +6221 1234 5678 <br />
+                            CS 1 : +62 812 1234 5678 <br />
+                            CS 2 : +62 813 1234 5678 <br />
+                            Website : <a href='http://www.cacan.id/' title='Website cacan'>www.cacan.id</a> <br />
+                            Email : <a href='mailto:support@cacan.id' title='E-mail cacan'>support@cacan.id</a> <br />";
+}
+
+/* 
+ * Created by cacan
+ * Email :rony.chandra.kudus@gmail.com
+ */
+
